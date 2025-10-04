64176b0e8b720172830feb6fd6e08bdf6460fdee
Logonz <Logon.q@gmail.com>
2025-10-04 19:52:06 +0200

    refactor(fadeDecorator): comment out unused decoration options

--- src/from-till/fadeDecorator.ts
+++ src/from-till/fadeDecorator.ts
@@ -6,8 +6,19 @@ function getDecoration(editor: vscode.TextEditor): vscode.TextEditorDecorationTy
   let decoration = decorationMap.get(editor);
   if (!decoration) {
     decoration = vscode.window.createTextEditorDecorationType({
-      isWholeLine: true,
+      // isWholeLine: true,
       color: "#FFFFFF55",
+      // color: "#FFFFFF",
+      // opacity: "0.5", // This works but also dimms the other decorations
+      // textDecoration: "none; opacity: 0.5", // This works but also dimms the other decorations
+      // before: {
+      //   width: "100%",
+      //   contentText: "_",
+      //   color: "#FFFFFF00",
+      //   backgroundColor: "#00000055",
+      //   // backgroundColor: "#FFFFFF55",
+      //   textDecoration: "none;position:absolute;z-index:999999;max-height:100%;opacity: 0.5;", // This works but also dimms the other decorations
+      // },
     });
     decorationMap.set(editor, decoration);
   }
